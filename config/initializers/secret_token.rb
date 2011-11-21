de9fbf31a7256fc34795ddaf3baae8f18b495b75
Andi Staub <andi.staub@gmail.com>
2011-11-21 09:07:46 +0100

    First commit of the new page.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CytPage::Application.config.secret_token = 'ba244fc2ad8e4f9e8ab2534d58e5331f6d8b1cf364882e844bf224c09109a6c08cf69815decb8bbf540b9e768df295eaf16624efe687f6f47984561939e2f64d'
