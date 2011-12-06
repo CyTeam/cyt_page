bd1dcfaf7a26d8f590983b0e3545dd06a5128346
Andi Staub <andi.staub@gmail.com>
2011-12-06 16:52:37 +0100

    Made hole lot of changes to the layout and startet to fill in the content.

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ CytPage::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'welcome#index'
+  match ':action', :controller => :welcome
 
   # See how all your routes lay out with "rake routes"
 
